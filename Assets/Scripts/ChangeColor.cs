f4b267718790427c96beddd0be6ec6e8b1097cfa
MasterQQ <aqquan2017@gmail.com>
2020-11-18 15:14:38 +0700

    Finish Demo Prototype

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void ChangeRandomColor()
+    {
+        Color c = new Color(Get01(), Get01(), Get01());
+        GetComponent<Renderer>().material.color = c;
+    }
+
+    private float Get01() => Random.Range(0f, 1f);
+}
