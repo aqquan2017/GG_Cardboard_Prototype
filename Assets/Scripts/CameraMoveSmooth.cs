24cb9dc949742f5a2a134d179a45ca3986db36c5
MasterQQ <aqquan2017@gmail.com>
2021-01-08 16:42:39 +0700

    Final

--- Assets/Scripts/CameraMoveSmooth.cs
+++ Assets/Scripts/CameraMoveSmooth.cs
@@ -6,13 +6,7 @@ public class CameraMoveSmooth : MonoBehaviour
 {
     public Transform A;
     public float speedSmooth;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.Lerp(transform.position, A.position, speedSmooth * Time.deltaTime);
